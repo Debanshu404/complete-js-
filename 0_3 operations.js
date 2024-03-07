2e73cbb8fc5e699b08e44b08b3958fadc36c810d
Debanshu404 <debanshupati12345@gmail.com>
2024-03-07 20:16:19 +0530

    Operations IN js

---
+++ 0_3 operations.js
@@ -0,0 +1,27 @@
+// ************************************************** OPERATIONS*********************************************
+
+// 1- WE CAN CREATE A NEGATIVE VALUE OF A POSTIVE NUMBER BUT THE CONVERSE CANT BE DONE !]
+let value = 69
+let negValue = -value;
+//  console.log(negValue)
+//   2-some more operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// 3-concatination of a string 
+
+let constring = "slayer"
+let greetString = " good morning"
+//  console.log(constring+greetString);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2) here js tries to concatinate every thing after 1 
+// console.log("1"+2)
+// console.log(1+2+"2") note: everytime when we use strings after a number or any data type it simply first evaluates the operation of the first data types and then concates with the string value😎
+// console.log(+true)
+// console.log(+false)
