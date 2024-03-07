2c2bb1a6de94a93506f4f9fa37187e3f8308db36
Debanshu404 <debanshupati12345@gmail.com>
2024-03-07 20:13:52 +0530

    Type conversion in js

---
+++ 0_2 type conversion.js
@@ -0,0 +1,12 @@
+// **************************************************************** type conversion*************************************************
+// for integers
+let valueOfInteger = "slayer"
+let score = Number(valueOfInteger);
+// console.log(score) this will print NaN in console which means not a number 
+// for boolenas
+let booleanIsLoggedIn = Boolean("flase")
+// console.log(booleanIsLoggedIn)
+
+// for string bhais
+
+let someNumber = 26
