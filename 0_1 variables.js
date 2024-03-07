6befeaa87487db361d07952daae7e2bb214dd953
Debanshu404 <debanshupati12345@gmail.com>
2024-03-07 20:10:29 +0530

    variables in js

---
+++ 0_1 variables.js
@@ -0,0 +1,49 @@
+// 1>=VARIABLES
+
+
+
+
+///////////////////////////////////////////////////////////
+// a=2;
+// b=a+6;
+// console.log(a+b)
+//////////////////////////////////////////////////////////////
+// Q-1create a button which onclickig it will ask your age and alert greetings to you with your name and welcomes you in the heading
+
+// let A = document.document.getElementById('button');
+// let B = document.document.getElementById("heading")
+
+// A.onclick = () => {
+//     const name = prompt("enter your name")
+//     alert(`hello ${name}, good morning `)
+//    heading.textContent = `Welcome sir ${name} to stark technology`;
+// };
+// const button = document.querySelector("#button");
+// const names = document.querySelector("#heading");
+
+// button.onclick = () => {
+//   let username = prompt("enter name");
+//   alert(`hello ${username},hi`);
+//   names.textContent = `hi ${username}, welcome`
+// }
+// Q-2 DIFFERENTIATE BETWEEN VAR AND LET BY A SIMPLE NAME PROGGRAME
+//  myname="debanshu"
+
+//  function nameop(){
+//     console.log(myname);
+//  }
+//  nameop()
+
+//  var myname; it will work
+// let myname; it will not work
+// var allows us to declare this variable as many times as we can in the entire code but let doesnt(this is called hoisting)
+
+
+let myName = ""
+myName = "debanshu"
+let age = 30
+age += 2;
+age -= 2;
+age /= 2;
+age *= 2;
+// console.log(age)
