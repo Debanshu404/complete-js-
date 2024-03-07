80c1f2d8e396beaffeddba072385c8af12e98cb6
Debanshu404 <debanshupati12345@gmail.com>
2024-03-07 20:16:04 +0530

    Comparisions in Js

---
+++ 0_4 comparisions.js
@@ -0,0 +1,20 @@
+// *****************************************  comparisions*************************************
+
+//  these operatos will always return value in booleans ie-in true or false
+// console.log(6 > 1);
+// console.log(5 >= 1);
+// console.log(4 < 1);
+// console.log(3 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); (returns true because js converts the 1 to a string for a comparision)
+// console.log("02" > 1);
+
+
+// console.log(null > 0); (returns flase as js treats null as =0 and 0!>0)
+// console.log(null == 0); in this case due to == there is no type correction hence we get flase and same for ===
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+//  console.log(undefined >= 0); returns flase as due to conversion it converts to NaN hence NaN !=0
+// console.log(undefined < 0);
