3ad46546f7233dd2695302f6d72f4fdd8dc379bc
Debanshu404 <debanshupati12345@gmail.com>
2024-03-07 20:15:53 +0530

    Data Types In Javascript

---
+++ 0_7 dataTypes in js.js
@@ -0,0 +1,27 @@
+// **********************************************datatypes in js ******************************************************
+
+
+// THERE AR EBASICALLY 2 TYPES OF DATATYPES IN JS 
+// 1-PRIMITIVE
+// 2-REFERENCE (NON-PRIMITIVE)
+//  PRIMITIVE👇
+//  7TYPES: String (""),Symbol(SYM),Boolean(TRUE OR FLASE),BigInt(FOR HANDLING BIG INTEGER VALUES),null(NULL),undefined(SOMETHING WHICH IS EMPTY) SHORTCUT SSBBNU
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(anotherId)
+// console.log(id === anotherId); best example for number symbol and boolean in one proggram.
+
+// NON PRIMITIVE DATA TYPES
+
+// ARRAYS OBJECTS FUNCTIONS
+//  FOR OBJECTS
+const obj = {
+
+  NAME: "rohan",
+  aGE: 20
+
+
+}
