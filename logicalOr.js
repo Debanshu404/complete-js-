b4432c65b246802f656dafd6077d5115fe45af47
Debanshu404 <debanshupati12345@gmail.com>
2024-03-18 22:18:49 +0530

    Create logicalOr.js

---
+++ logicalOr.js
@@ -0,0 +1,22 @@
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+   
+  
+    if (val>10 || val <20) {
+      return "inside";
+    }
+  
+    // Only change code above this line
+    return "outside";
+  }
+  console.log(testLogicalOr(10))
+  console.log(testLogicalOr(0) )
+  
+  console.log(testLogicalOr(9) )
+  console.log(testLogicalOr(10)) 
+  console.log(testLogicalOr(15)) 
+  console.log(testLogicalOr(19)) 
+  console.log(testLogicalOr(20)) 
+  console.log(testLogicalOr(21)) 
+  console.log(testLogicalOr(25))
